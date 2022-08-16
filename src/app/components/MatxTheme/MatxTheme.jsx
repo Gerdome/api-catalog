ae2a5bff67e2af5c2de7de349dca79243879cef3
Gerdome <merz.gerrit@gmail.com>
2022-08-16 13:59:08 +0200

    change theme

--- src/app/components/MatxTheme/MatxTheme.jsx
+++ src/app/components/MatxTheme/MatxTheme.jsx
@@ -1,27 +1,12 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import useSettings from 'app/hooks/useSettings';
-import { createTheme } from '@mui/material/styles';
-
-let theme = createTheme({
-  palette: {
-    background: {
-      default: "#0052cc",
-    },
-    primary: {
-      main: '#0052cc',
-    },
-    secondary: {
-      main: '#edf2ff',
-    },
-  },
-});
 
 const MatxTheme = ({ children }) => {
   const { settings } = useSettings();
   let activeTheme = { ...settings.themes[settings.activeTheme] };
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={activeTheme}>
       <CssBaseline />
       {children}
     </ThemeProvider>
