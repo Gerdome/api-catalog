89074b06921f8cc862d2d11d8121c891104a62b8
Gerdome <merz.gerrit@gmail.com>
2022-08-16 16:20:32 +0200

    correct root render for react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
 import { CssBaseline } from '@mui/material';
 import { StyledEngineProvider } from '@mui/styled-engine';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StyledEngineProvider injectFirst>
     <BrowserRouter>
       <CssBaseline />
       <App />
     </BrowserRouter>
   </StyledEngineProvider>,
-  document.getElementById('root')
 );
 
 // for IE-11 support un-comment cssVars() and it's import in this file
