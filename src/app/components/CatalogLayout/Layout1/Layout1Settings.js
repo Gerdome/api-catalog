e71ffd40edd23e8fc3c21cc7337f97a620e18f9b
Gerdome <merz.gerrit@gmail.com>
2022-08-16 17:37:41 +0200

    renaming

--- src/app/components/CatalogLayout/Layout1/Layout1Settings.js
+++ src/app/components/CatalogLayout/Layout1/Layout1Settings.js
@@ -2,13 +2,13 @@ const Layout1Settings = {
   leftSidebar: {
     show: true,
     mode: 'full', // full, close, compact, mobile,
-    theme: 'whiteDarkred', // View all valid theme colors inside MatxTheme/themeColors.js
+    theme: 'whiteDarkred', // View all valid theme colors inside CatalogTheme/themeColors.js
     bgImgURL: '/assets/images/sidebar/sidebar-bg-dark.jpg',
   },
   topbar: {
     show: true,
     fixed: true,
-    theme: 'whiteDarkred', // View all valid theme colors inside MatxTheme/themeColors.js
+    theme: 'whiteDarkred', // View all valid theme colors inside CatalogTheme/themeColors.js
   },
 };
 
--- src/app/components/CatalogLayout/settings.js
+++ src/app/components/CatalogLayout/settings.js
@@ -2,10 +2,9 @@ import { themes } from '../CatalogTheme/initThemes';
 import layout1Settings from './Layout1/Layout1Settings';
 
 // UPDATE BELOW CODE
-// DOC http://demos.ui-lib.com/matx-react-doc/layout.html
 export const CatalogLayoutSettings = {
   activeLayout: 'layout1', // layout1, layout2
-  activeTheme: 'helsana', // View all valid theme colors inside MatxTheme/themeColors.js
+  activeTheme: 'helsana', // View all valid theme colors inside CatalogTheme/themeColors.js
   perfectScrollbar: false,
 
   themes: themes,
@@ -14,12 +13,12 @@ export const CatalogLayoutSettings = {
   secondarySidebar: {
     show: false,
     open: false,
-    theme: 'whiteDarkred', // View all valid theme colors inside MatxTheme/themeColors.js
+    theme: 'whiteDarkred', // View all valid theme colors inside CatalogTheme/themeColors.js
   },
   // Footer options
   footer: {
     show: false,
     fixed: false,
-    theme: 'helsana', // View all valid theme colors inside MatxTheme/themeColors.js
+    theme: 'helsana', // View all valid theme colors inside CatalogTheme/themeColors.js
   },
 };
