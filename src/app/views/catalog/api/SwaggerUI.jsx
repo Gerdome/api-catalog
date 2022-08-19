c11b7548227ebd6b9e409379832d1d5fe4aede40
Gerdome <merz.gerrit@gmail.com>
2022-08-19 11:03:14 +0200

    added dockerfile and changing deps

--- src/app/views/catalog/api/SwaggerUI.jsx
+++ src/app/views/catalog/api/SwaggerUI.jsx
@@ -1,14 +1,14 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 
-import SwaggerUI from "swagger-ui-react"
-import "swagger-ui-react/swagger-ui.css"
+/* import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css" */
 
 const SwaggerGUI = ({openApiSpec}) => {
     return (
         <Grid container spacing={3}>
             <Grid item lg={12} md={12} sm={12} xs={12}>
-                <SwaggerUI url={openApiSpec} />
+                {/* <SwaggerUI url={openApiSpec} /> */}
             </Grid>
         </Grid>
 
